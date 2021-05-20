0cf642d89ce0f51dfa9ca1fb762857ad36d15c64
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-05-20 23:20:31 +0300

    Task03 added

--- by.epam.unit03.main/src/by/epam/unit03/main/Task03.java
+++ by.epam.unit03.main/src/by/epam/unit03/main/Task03.java
@@ -2,6 +2,19 @@ package by.epam.unit03.main;
 
 public class Task03 {
     public static void main(String[] args) {
+        //Calculate: (1+2)*(1+2+3)*...*(1+2+...+10)
+
+        int x = 1;
+        int bracketsSum = 0;
+        long total = 1;
+
+        while (x <= 10) {
+            bracketsSum = bracketsSum + x;
+            total = total * bracketsSum;
+            x++;
+
+        }
+        System.out.println("The result value is " + total);
 
     }
 }

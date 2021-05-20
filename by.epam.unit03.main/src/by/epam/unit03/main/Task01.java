a8421f3aa8757076bcfb4277b42431d86b9e9994
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-05-20 20:39:54 +0300

    Task01 added

--- by.epam.unit03.main/src/by/epam/unit03/main/Task01.java
+++ by.epam.unit03.main/src/by/epam/unit03/main/Task01.java
@@ -3,5 +3,18 @@ package by.epam.unit03.main;
 public class Task01 {
     public static void main(String[] args) {
 
+        //Display the multiplication table by 3
+
+        int x = 3;
+        int y = 1;
+
+        System.out.println("The multiplication table by 3 is:");
+
+        while(y <= 10) {
+            System.out.println("3 * " + y + " = " + x * y);
+            y++;
+
+        }
+
     }
 }

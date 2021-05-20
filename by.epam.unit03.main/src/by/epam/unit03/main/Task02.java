da6b98fe58fd1ce1b70159319096875c601c9966
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-05-20 21:22:04 +0300

    Task02 added

--- by.epam.unit03.main/src/by/epam/unit03/main/Task02.java
+++ by.epam.unit03.main/src/by/epam/unit03/main/Task02.java
@@ -3,5 +3,19 @@ package by.epam.unit03.main;
 public class Task02 {
     public static void main(String[] args) {
 
+        //Use the while operator to determine the sum of all odd numbers in the range from 1 to 99 inclusive
+
+        int x = 1;
+        int sum = 0;
+
+        while (x <= 99) {
+            if (x % 2 != 0) {
+                sum = sum + x;
+            }
+            x++;
+
+        }
+        System.out.println("The sum of all odd numbers in the range from 1 to 99 inclusive = " + sum);
     }
+
 }

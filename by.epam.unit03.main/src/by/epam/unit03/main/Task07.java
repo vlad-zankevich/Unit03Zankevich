577874d2143a6ecf6a135a2de1d0b525d787e178
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-05-23 19:24:49 +0300

    Task 7 added

--- by.epam.unit03.main/src/by/epam/unit03/main/Task07.java
+++ by.epam.unit03.main/src/by/epam/unit03/main/Task07.java
@@ -1,7 +1,41 @@
 package by.epam.unit03.main;
 
+import java.util.Scanner;
+
 public class Task07 {
     public static void main(String[] args) {
+        //Write a program that converts Roman numbers to Arabic numbers.
+
+        String a;
+        System.out.println("Please enter only one Roman number in upper case: I, V, X, L, C, D, M\n>");
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextLine();
+
+        switch (a) {
+            case "I":
+                System.out.println("The Roman I is 1");
+                break;
+            case "V":
+                System.out.println("The Roman V is 5");
+                break;
+            case "X":
+                System.out.println("The Roman X is 10");
+                break;
+            case "L":
+                System.out.println("The Roman L is 50");
+                break;
+            case "C":
+                System.out.println("The Roman C is 100");
+                break;
+            case "D":
+                System.out.println("The Roman D is 500");
+                break;
+            case "M":
+                System.out.println("The Roman M is 1000");
+                break;
+            default:
+                System.out.println("Sorry, but there are no such Roman numbers");
+        }
 
     }
 }

a56e6021acdac96d291f44c49060cb732a3946cb
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-05-23 13:45:05 +0300

    Update Task04.java

--- by.epam.unit03.main/src/by/epam/unit03/main/Task04.java
+++ by.epam.unit03.main/src/by/epam/unit03/main/Task04.java
@@ -2,6 +2,18 @@ package by.epam.unit03.main;
 
 public class Task04 {
     public static void main(String[] args) {
+        double x = -5;
+        double y;
+        System.out.println("-------------------------");
+        System.out.printf("|%8s\t|%8s\t|\n", "x", "y");
+        System.out.println("-------------------------");
+
+        while(x <= 5) {
+            y = 5 - (Math.pow(x, 2)) / 2;
+            System.out.printf("|\t%4.1f\t|\t%4.1f\t|\n", x, y);
+            x = x + 0.5;
+        }
+        System.out.println("-------------------------");
 
     }
 }
